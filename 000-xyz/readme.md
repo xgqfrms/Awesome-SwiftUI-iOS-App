3957b51803e7dd1ea4ac0938c1d30f454f6477f1
xgqfrms <xgqfrms@outlook.com>
2020-10-28 22:58:00 +0800

    init SwiftUI app

---
+++ 000-xyz/readme.md
@@ -0,0 +1,15 @@
+# SwiftUI
+
+> iOS & Swift
+
+https://developer.apple.com/documentation/swiftui/
+
+https://developer.apple.com/xcode/swiftui/
+
+
+## refs
+
+> Stanford CS193p - Developing Apps for iOS
+
+https://cs193p.sites.stanford.edu/
+
---
+++ src/MemorizeGame/MemorizeGame/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  MemorizeGame
+//
+//  Created by 夏凌晨 on 2020/10/28.
+//  Copyright © 2020 xgqfrms. All rights reserved.
+//
+
+import SwiftUI
+
+// ✅ some View，返回任意类型的 View, 自动推断 View 类型
+//struct ContentView: View {
+//  var body: some View {
+//    Text("Hello, World!")
+//  }
+//}
+
+// 👎 Text 返回 Text 类型的 View, 显示指定返回的 View 类型
+struct ContentView: View {
+  var body: Text {
+    // Text("Hello, SwiftUI!")
+    // return 可以省略
+    return Text("Hello, SwiftUI!")
+  }
+}
+
+
+
+
+
+
+
+//  PreviewProvider 仅用于预览，代码在项目中没有实际的意义
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
